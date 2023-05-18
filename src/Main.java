6260836f8d7efd62a977e1ba77b963115b9baa71
Lucas-Ramos <drawingworldab@gail.com>
2023-05-18 07:58:30 -0300

    codigos adicionados

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+       int cont = 0;
+       do{
+           cont++;
+           System.out.println(cont);
+       }while(cont < 10);
+
+       int contador = 0;
+       while(contador < 10){
+           contador++;
+           System.out.println(contador);
+       }
+
+       for(int x = 0; x<10; x++){
+            System.out.println(x);
+       }
     }
 }
